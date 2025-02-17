7a71be5da3d9dd2887f9ffacc7640858598e7533
Rohit Tudu <rohittudu181@gmail.com>
2025-02-17 20:47:43 +0530

    First Commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: ["plus.unsplash.com"], // For external images
+    unoptimized: true, // Serve images as is (useful for local SVGs)
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"], // Allows importing SVGs as React components
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
